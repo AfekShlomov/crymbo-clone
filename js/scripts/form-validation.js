599b7fe8eac2970433c8149fa0d2bfa6e61f3497
Afek Shlomov <afekapple182003@gmail.com>
2025-06-11 16:25:51 +0300

    form validation error message typing fix

--- js/scripts/form-validation.js
+++ js/scripts/form-validation.js
@@ -1,5 +1,5 @@
 const form = document.querySelector(".form-main-div form");
-const fields = form.querySelectorAll("input, textarea");
+const fields = form.querySelectorAll('input:not([type="checkbox"]), textarea');
 const errorMessages = form.querySelectorAll(".error-message");
 const confirmationMessage = document.querySelector(".confirmation-message");
 const connectCheckbox = form.querySelector('input[name="connect-checkbox"]');
@@ -7,60 +7,61 @@ const oracleCheckbox = form.querySelector('input[name="oracle-checkbox"]');
 const checkboxError = connectCheckbox
   .closest(".form-main-div-checkbox")
   .querySelector(".error-message");
-
 const formData = {};
+
+updateFormData();
+
+fields.forEach((field) => {
+  field.addEventListener("input", () => {
+    updateFormData();
+    const key = field.name;
+    console.log(formData[key]);
+    validateErrorMessage(formData[key]);
+  });
+});
 
 function handleSubmit(event) {
   event.preventDefault();
-
-  formData["checkboxes"] = {
-    connect: connectCheckbox.checked,
-    oracle: oracleCheckbox.checked,
-    errorMessage: checkboxError,
-  };
-
-  fields.forEach((field) => {
-    if (field.type !== "checkbox") {
-      const key = field.name;
-      const value = field.value.trim();
-      const errorMessage = field.parentElement.querySelector(".error-message");
-      formData[key] = { value, errorMessage };
-    }
-  });
+  updateFormData();
 
   Object.entries(formData).forEach(([key, data]) => {
     data.errorMessage.style.display = "none";
     if (data.value === "" && key !== "job" && key !== "information") {
-      data.errorMessage.textContent = "This field is required.";
-      data.errorMessage.style.display = "block";
+      updateErrorMessage(data.errorMessage, "This field is required");
     } else if (key === "email") {
       const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!emailPattern.test(data.value)) {
-        data.errorMessage.textContent = "Please enter a valid email address.";
-        data.errorMessage.style.display = "block";
+        updateErrorMessage(
+          data.errorMessage,
+          "Please enter a valid email address"
+        );
       }
     } else if (["first name", "last name"].includes(key)) {
       const namePattern = /^[a-zA-Z\s]+$/;
       if (!namePattern.test(data.value) || data.value.length < 2) {
-        data.errorMessage.textContent = "Please enter a valid " + key + ".";
-        data.errorMessage.style.display = "block";
+        updateErrorMessage(data.errorMessage, "Please enter a valid " + key);
       }
     } else if (key === "company") {
       const namePattern = /^[a-zA-Z0-9\s]+$/;
       if (!namePattern.test(data.value) || data.value.length < 2) {
-        data.errorMessage.textContent = "Please enter a valid company name.";
-        data.errorMessage.style.display = "block";
+        updateErrorMessage(
+          data.errorMessage,
+          "Please enter a valid company name."
+        );
       }
     } else if (key === "information" && data.value !== "") {
       if (data.value.length < 10) {
-        data.errorMessage.textContent =
-          "information must include at least 10 characters.";
-        data.errorMessage.style.display = "block";
+        updateErrorMessage(
+          data.errorMessage,
+          "information must include at least 10 characters"
+        );
       }
     } else if (key === "checkboxes") {
       if (!data.connect && !data.oracle) {
-        data.errorMessage.textContent = "please select at least one use case";
-        data.errorMessage.style.display = "block";
+        updateErrorMessage(
+          data.errorMessage,
+          "please select at least one use case"
+        );
       }
     }
   });
@@ -77,6 +78,40 @@ function handleSubmit(event) {
     }, 5000);
     return;
   }
+}
+
+function updateFormData() {
+  formData["checkboxes"] = {
+    connect: connectCheckbox.checked,
+    oracle: oracleCheckbox.checked,
+    errorMessage: checkboxError,
+  };
+
+  fields.forEach((field) => {
+    const key = field.name;
+    const value = field.value.trim();
+    const errorMessage = field.parentElement.querySelector(".error-message");
+    formData[key] = { value, errorMessage };
+  });
+}
+
+function validateErrorMessage(formDataItem) {
+  // for when an input is being typed in, disables error message
+
+  console.log(formDataItem)
+
+  if (formDataItem.value === "") {
+    updateErrorMessage(formDataItem.errorMessage, "This field is required");
+  } else {
+    updateErrorMessage(formDataItem.errorMessage, "");
+  }
+}
+
+function updateErrorMessage(errorElement, message) {
+  // update error messages during updates
+
+  errorElement.textContent = message;
+  errorElement.style.display = message === "" ? "none" : "block";
 }
 
 const submitButton = document.getElementById("submitButton");
