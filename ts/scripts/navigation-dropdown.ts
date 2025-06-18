7973e95df7a1234604b58f5dd6a1d5dde0c8b0ab
Afek Shlomov <afekapple182003@gmail.com>
2025-06-18 18:53:15 +0300

    nav dropdown to ts

---
+++ ts/scripts/navigation-dropdown.ts
@@ -0,0 +1,14 @@
+export {};
+
+const menu = document.querySelector(".header-dropdown-navigation") as HTMLDivElement;
+const menuButton = document.querySelector("#dropdown-menu-button") as HTMLButtonElement;
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+
+const logoDiv = document.querySelector(".logo-div") as HTMLDivElement;
+logoDiv.addEventListener("click", () => {
+  window.location.href = "/";
+});
+
