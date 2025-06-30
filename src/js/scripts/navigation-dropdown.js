90c9c96cc0bcfc16799278805f32a3a3c38936f1
Afek Shlomov <afekapple182003@gmail.com>
2025-06-30 14:49:46 +0300

    header BEM and scss fix

--- src/js/scripts/navigation-dropdown.js
+++ src/js/scripts/navigation-dropdown.js
@@ -1,11 +1,13 @@
-const menu = document.querySelector(".header-dropdown-navigation");
-const menuButton = document.querySelector("#dropdown-menu-button");
+const menu = document.querySelector(".dropdown-container");
+const menuButton = document.querySelector(".header--dropdown-menu-button");
+
+console.log(menu, menuButton);
 
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
 
-const logoDiv = document.querySelector(".logo-div");
+const logoDiv = document.querySelector(".header__logo-container");
 logoDiv.addEventListener("click", () => {
   window.location.href = "/";
 });
