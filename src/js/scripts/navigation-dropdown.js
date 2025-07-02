c19a0c8247d7f775ca993a7864b29a587924f222
Afek Shlomov <afekapple182003@gmail.com>
2025-07-02 13:20:17 +0300

    change header logo a link

--- src/js/scripts/navigation-dropdown.js
+++ src/js/scripts/navigation-dropdown.js
@@ -4,8 +4,3 @@ const menuButton = document.querySelector(".header--dropdown-menu-button");
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
-
-const logoDiv = document.querySelector(".header__logo-container");
-logoDiv.addEventListener("click", () => {
-  window.location.href = "/";
-});
