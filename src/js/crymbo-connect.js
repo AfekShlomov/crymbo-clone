1304ae0086bc7545ed529c5824c8bc8245d52e60
Afek Shlomov <afekapple182003@gmail.com>
2025-07-02 11:41:54 +0300

    fix value full-value bs

--- src/js/crymbo-connect.js
+++ src/js/crymbo-connect.js
@@ -1,5 +1,5 @@
 import "../scss/pages/crymbo-connect.scss";
 import "../js/scripts/connect-quotes-cycle";
-import "../js/scripts/connect-value-cycling";
+// import "../js/scripts/connect-value-cycling";
 import "../js/scripts/navigation-dropdown";
 import "./scripts/layout-shift";
