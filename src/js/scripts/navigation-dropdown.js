1b4e905bf17cdd98fe83c58a9822aca436ac1dbb
Afek Shlomov <afekapple182003@gmail.com>
2025-07-02 16:04:01 +0300

    BEM optimization footer header

--- src/js/scripts/navigation-dropdown.js
+++ src/js/scripts/navigation-dropdown.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector(".dropdown-container");
-const menuButton = document.querySelector(".header__dropdown-menu-button");
+const menuButton = document.querySelector(
+  ".header__buttons-container__dropdown-menu-button"
+);
 
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("open");
